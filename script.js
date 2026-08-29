417206446deb434610a8fc0cec16dc5bdb47efa5
AmeyaBanjara <ameyabanjara11@gmail.com>
2026-08-29 17:21:04 +0000

    org

--- script.js
+++ script.js
@@ -2,12 +2,3 @@ function chooseActivity(activity) {
     document.getElementById("result").innerText =
         "You chose: " + activity;
 }
-
-function showBeachOptions() {
-    document.getElementById("beach-options").style.display = "block";
-}
-
-function chooseBeach(person) {
-    document.getElementById("result").innerText =
-        "You chose: Beach with " + person;
-}
